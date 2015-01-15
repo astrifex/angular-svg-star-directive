f401e1f518080a0b29ca84fe6f027d58fa1130e4
Mike Dillon <mike@embody.org>
2015-01-14 22:42:20 -0800

    Fix indentation

--- grunt/ngAnnotate.js
+++ grunt/ngAnnotate.js
@@ -3,10 +3,10 @@
 module.exports = function ngAnnotate(grunt, options) {
   return {
     options: {},
-      dist: {
-        files: {
-          'dist/angular-svg-star-directive.js': [ 'dist/angular-svg-star-directive.pre.js' ]
-        }
+    dist: {
+      files: {
+        'dist/angular-svg-star-directive.js': [ 'dist/angular-svg-star-directive.pre.js' ]
+      }
     }
   };
 };
